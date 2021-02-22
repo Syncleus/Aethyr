2cd75f3e1b485b698605d9eeea3c7117a469b8f3
Jeffrey Phillips Freeman <jeffrey.freeman@syncleus.com>
2021-02-22 02:20:56 -0500

    added more to lexicon

--- lib/aethyr/core/objects/traits/lexicon.rb
+++ lib/aethyr/core/objects/traits/lexicon.rb
@@ -34,7 +34,6 @@ module Lexicon
   module Plurality
     PLURAL = :plural
     SINGULAR = :singular
-    NONE = :none
   end
 
   module GramaticalPerson
@@ -42,6 +41,7 @@ module Lexicon
     SECOND_PERSON = :second_person
     THIRD_PERSON = :third_person
     FOURTH_PERSON = :fourth_person
+    INTERROGATIVE = : :interrogative
   end
 
   module Subjectivity
@@ -49,10 +49,14 @@ module Lexicon
     SUBJECTIVE = :subjective
   end
 
-  module Reference
+  module Relation
+    NONE = :none
     REFLECTIVE = :reflective
     POSSESSIVE = :possessive
-    INTERROGATIVE = :interrogative
+  end
+
+  module Definitive
+    DEFINITE = :definite
     INDEFINITE = :indefinite
   end
 
@@ -61,18 +65,43 @@ module Lexicon
   def construct_lexicon(vocabulary)
   end
 
-  construct_lexicon([Vocab.new(word: 'I', plurality: Plurality::SINGULAR, gramatical_person: GramaticalPerson::FIRST_PERSON,
+  construct_lexicon([
+                     Vocab.new(word: 'I',        plurality: Plurality::SINGULAR, gramatical_person: GramaticalPerson::FIRST_PERSON,  relation: Relation::NONE,       subjectivity: Subjectivity::SUBJECTIVE),
+                     Vocab.new(word: 'me',       plurality: Plurality::SINGULAR, gramatical_person: GramaticalPerson::FIRST_PERSON,  relation: Relation::NONE,       subjectivity: Subjectivity::OBJECTIVE),
+                     Vocab.new(word: 'my',       plurality: Plurality::SINGULAR, gramatical_person: GramaticalPerson::FIRST_PERSON,  relation: Relation::POSSESSIVE, subjectivity: Subjectivity::SUBJECTIVE),
+                     Vocab.new(word: 'mine',     plurality: Plurality::SINGULAR, gramatical_person: GramaticalPerson::FIRST_PERSON,  relation: Relation::POSSESSIVE, subjectivity: Subjectivity::OBJECTIVE),
+                     Vocab.new(word: 'myself',   plurality: Plurality::SINGULAR, gramatical_person: GramaticalPerson::FIRST_PERSON,  relation: Relation::REFLEXIVE,  subjectivity: Subjectivity::OBJECTIVE),
+                     Vocab.new(word: 'you',      plurality: Plurality::SINGULAR, gramatical_person: GramaticalPerson::SECOND_PERSON, relation: Relation::NONE),
+                     Vocab.new(word: 'your',     plurality: Plurality::SINGULAR, gramatical_person: GramaticalPerson::SECOND_PERSON, relation: Relation::POSSESSIVE, subjectivity: Subjectivity::SUBJECTIVE),
+                     Vocab.new(word: 'yours',    plurality: Plurality::SINGULAR, gramatical_person: GramaticalPerson::SECOND_PERSON, relation: Relation::POSSESSIVE, subjectivity: Subjectivity::OBJECTIVE),
+                     Vocab.new(word: 'yourself', plurality: Plurality::SINGULAR, gramatical_person: GramaticalPerson::SECOND_PERSON, relation: Relation::REFLECTIVE, subjectivity: Subjectivity::OBJECTIVE),
+                     Vocab.new(word: 'she',      plurality: Plurality::SINGULAR, gramatical_person: GramaticalPerson::THIRD_PERSON,  relation: Relation::NONE,       subjectivity: Subjectivity::SUBJECTIVE, gender: Gender::FEMANINE),
+                     Vocab.new(word: 'her',      plurality: Plurality::SINGULAR, gramatical_person: GramaticalPerson::THIRD_PERSON,  relation: Relation::NONE,       subjectivity: Subjectivity::OBJECTIVE,  gender: Gender::FEMANINE),
+                     Vocab.new(word: 'her',      plurality: Plurality::SINGULAR, gramatical_person: GramaticalPerson::THIRD_PERSON,  relation: Relation::POSSESSIVE, subjectivity: Subjectivity::SUBJECTIVE, gender: Gender::FEMANINE),
+                     Vocab.new(word: 'hers',     plurality: Plurality::SINGULAR, gramatical_person: GramaticalPerson::THIRD_PERSON,  relation: Relation::POSSESSIVE, subjectivity: Subjectivity::OBJECTIVE,  gender: Gender::FEMANINE),
+                     Vocab.new(word: 'herself',  plurality: Plurality::SINGULAR, gramatical_person: GramaticalPerson::THIRD_PERSON,  relation: Relation::REFLECTIVE, subjectivity: Subjectivity::OBJECTIVE,  gender: Gender::FEMANINE),
+                     Vocab.new(word: 'he',       plurality: Plurality::SINGULAR, gramatical_person: GramaticalPerson::THIRD_PERSON,  relation: Relation::NONE,       subjectivity: Subjectivity::SUBJECTIVE, gender: Gender::MASCULIN),
+                     Vocab.new(word: 'him',      plurality: Plurality::SINGULAR, gramatical_person: GramaticalPerson::THIRD_PERSON,  relation: Relation::NONE,       subjectivity: Subjectivity::OBJECTIVE,  gender: Gender::MASCULIN),
+                     Vocab.new(word: 'his',      plurality: Plurality::SINGULAR, gramatical_person: GramaticalPerson::THIRD_PERSON,  relation: Relation::POSSESSIVE,                                         gender: Gender::MASCULIN),
+                     Vocab.new(word: 'himself',  plurality: Plurality::SINGULAR, gramatical_person: GramaticalPerson::THIRD_PERSON,  relation: Relation::REFLECTIVE, subjectivity: Subjectivity::OBJECTIVE,  gender: Gender::MASCULIN),
+                     Vocab.new(word: 'one',      plurality: Plurality::SINGULAR, gramatical_person: GramaticalPerson::FOURTH_PERSON, relation: Relation::NONE,       subjectivity: Subjectivity::SUBJECTIVE),
+                     Vocab.new(word: 'oneself',  plurality: Plurality::SINGULAR, gramatical_person: GramaticalPerson::FOURTH_PERSON, relation: Relation::REFLECTIVE, subjectivity: Subjectivity::OBJECTIVE),
+                     Vocab.new(word: 'who',      plurality: Plurality::SINGULAR, gramatical_person: GramaticalPerson::INTERROGATIVE, relation: Relation::NONE,       subjectivity: Subjectivity::OBJECTIVE),
+                     Vocab.new(word: 'whom',     plurality: Plurality::SINGULAR, gramatical_person: GramaticalPerson::INTERROGATIVE, relation: Relation::NONE,       subjectivity: Subjectivity::SUBJECTIVE),
+                     Vocab.new(word: 'whose',    plurality: Plurality::SINGULAR, gramatical_person: GramaticalPerson::INTERROGATIVE, relation: Relation::POSSESSIVE),
+                    ])
 end
 
-# interrogative pronounds:who, what, why, where, when, whatever
+# interrogative [subjective] pronounds:who, what, why, where, when, whatever
+# interrogative [objective] pronounds:whom, what, why, where, when, whatever
 # indefinate pronounds: anything, anybody, anyone, something, somebody, someone, nothing, nobody, none, no one
 # someone, something
 # direct-reflexive-pronoun: himself
 # indirect-reflective-pronoun: me
 
-# reflexive pronouns: myself, yourself, himself, herself, itself, ourselves, yourselves, themselves.
-# possessive adjective/determiner: my, your, his, her, its, our, their
-# possessive pronoun: mine, yours, his, hers, its, ours, theirs
+# reflexive [objective] pronouns: myself, yourself, himself, herself, itself, ourselves, yourselves, themselves.
+# possessive [subjective] adjective/determiner: my, your, his, her, its, our, their
+# possessive [objective]  pronoun: mine, yours, his, hers, its, ours, theirs
 # objective pronoun: me, us, you, him, her, it, them, and whom
 # subjective pronoun:  I, you, we, he, she, it, they, and who
 
